6536d26a49efe5399b70a4d89e5d8e00b9062de3
ashirafzal <ashirafzal96@gmail.com>
2019-08-26 16:04:45 +0500

    product Price Searching Code Added in the Product Price form

--- Foodies/ProductPrice.Designer.cs
+++ Foodies/ProductPrice.Designer.cs
@@ -28,17 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.InvoiceNumber = new System.Windows.Forms.TextBox();
-            this.SearchInvoice = new System.Windows.Forms.Button();
+            this.txtSearchPrice = new System.Windows.Forms.TextBox();
+            this.SearchPrice = new System.Windows.Forms.Button();
             this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.productsDataSet = new Foodies.ProductsDataSet();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productsTableAdapter = new Foodies.ProductsDataSetTableAdapters.ProductsTableAdapter();
+            this.productIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productPriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productCategoryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productImageDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -87,14 +98,15 @@
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.04678F));
             this.tableLayoutPanel2.Size = new System.Drawing.Size(1600, 791);
             this.tableLayoutPanel2.TabIndex = 1;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
             // 
             // tableLayoutPanel3
             // 
             this.tableLayoutPanel3.ColumnCount = 2;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.10474F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.89526F));
-            this.tableLayoutPanel3.Controls.Add(this.SearchInvoice, 0, 0);
-            this.tableLayoutPanel3.Controls.Add(this.InvoiceNumber, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.SearchPrice, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.txtSearchPrice, 0, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
@@ -105,31 +117,32 @@
             this.tableLayoutPanel3.Size = new System.Drawing.Size(1600, 54);
             this.tableLayoutPanel3.TabIndex = 0;
             // 
-            // InvoiceNumber
+            // txtSearchPrice
             // 
-            this.InvoiceNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.InvoiceNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InvoiceNumber.Location = new System.Drawing.Point(28, 6);
-            this.InvoiceNumber.Margin = new System.Windows.Forms.Padding(20, 5, 0, 5);
-            this.InvoiceNumber.Multiline = true;
-            this.InvoiceNumber.Name = "InvoiceNumber";
-            this.InvoiceNumber.Size = new System.Drawing.Size(1293, 42);
-            this.InvoiceNumber.TabIndex = 1;
-            this.InvoiceNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtSearchPrice.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtSearchPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchPrice.Location = new System.Drawing.Point(28, 6);
+            this.txtSearchPrice.Margin = new System.Windows.Forms.Padding(20, 5, 0, 5);
+            this.txtSearchPrice.Multiline = true;
+            this.txtSearchPrice.Name = "txtSearchPrice";
+            this.txtSearchPrice.Size = new System.Drawing.Size(1293, 42);
+            this.txtSearchPrice.TabIndex = 1;
+            this.txtSearchPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // SearchInvoice
+            // SearchPrice
             // 
-            this.SearchInvoice.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.SearchInvoice.BackColor = System.Drawing.Color.RoyalBlue;
-            this.SearchInvoice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.SearchInvoice.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SearchInvoice.ForeColor = System.Drawing.Color.White;
-            this.SearchInvoice.Location = new System.Drawing.Point(1337, 3);
-            this.SearchInvoice.Name = "SearchInvoice";
-            this.SearchInvoice.Size = new System.Drawing.Size(254, 48);
-            this.SearchInvoice.TabIndex = 2;
-            this.SearchInvoice.Text = "SEARCH PRICE";
-            this.SearchInvoice.UseVisualStyleBackColor = false;
+            this.SearchPrice.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.SearchPrice.BackColor = System.Drawing.Color.RoyalBlue;
+            this.SearchPrice.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SearchPrice.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchPrice.ForeColor = System.Drawing.Color.White;
+            this.SearchPrice.Location = new System.Drawing.Point(1337, 3);
+            this.SearchPrice.Name = "SearchPrice";
+            this.SearchPrice.Size = new System.Drawing.Size(254, 48);
+            this.SearchPrice.TabIndex = 2;
+            this.SearchPrice.Text = "SEARCH PRICE";
+            this.SearchPrice.UseVisualStyleBackColor = false;
+            this.SearchPrice.Click += new System.EventHandler(this.SearchPrice_Click);
             // 
             // dgv1
             // 
@@ -138,15 +151,74 @@
             this.dgv1.AllowUserToResizeColumns = false;
             this.dgv1.AllowUserToResizeRows = false;
             this.dgv1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dgv1.AutoGenerateColumns = false;
             this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgv1.BackgroundColor = System.Drawing.Color.White;
             this.dgv1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productIdDataGridViewTextBoxColumn,
+            this.productNameDataGridViewTextBoxColumn,
+            this.productPriceDataGridViewTextBoxColumn,
+            this.productCategoryDataGridViewTextBoxColumn,
+            this.productImageDataGridViewImageColumn});
+            this.dgv1.DataSource = this.productsBindingSource;
             this.dgv1.Location = new System.Drawing.Point(29, 103);
             this.dgv1.Name = "dgv1";
             this.dgv1.ReadOnly = true;
             this.dgv1.Size = new System.Drawing.Size(1541, 639);
             this.dgv1.TabIndex = 2;
+            // 
+            // productsDataSet
+            // 
+            this.productsDataSet.DataSetName = "ProductsDataSet";
+            this.productsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.productsDataSet;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // productIdDataGridViewTextBoxColumn
+            // 
+            this.productIdDataGridViewTextBoxColumn.DataPropertyName = "ProductId";
+            this.productIdDataGridViewTextBoxColumn.HeaderText = "PRODUCT ID";
+            this.productIdDataGridViewTextBoxColumn.Name = "productIdDataGridViewTextBoxColumn";
+            this.productIdDataGridViewTextBoxColumn.ReadOnly = true;
+            this.productIdDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // productNameDataGridViewTextBoxColumn
+            // 
+            this.productNameDataGridViewTextBoxColumn.DataPropertyName = "ProductName";
+            this.productNameDataGridViewTextBoxColumn.HeaderText = "PRODUCT NAME";
+            this.productNameDataGridViewTextBoxColumn.Name = "productNameDataGridViewTextBoxColumn";
+            this.productNameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // productPriceDataGridViewTextBoxColumn
+            // 
+            this.productPriceDataGridViewTextBoxColumn.DataPropertyName = "ProductPrice";
+            this.productPriceDataGridViewTextBoxColumn.HeaderText = "PRODUCT PRICE";
+            this.productPriceDataGridViewTextBoxColumn.Name = "productPriceDataGridViewTextBoxColumn";
+            this.productPriceDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // productCategoryDataGridViewTextBoxColumn
+            // 
+            this.productCategoryDataGridViewTextBoxColumn.DataPropertyName = "ProductCategory";
+            this.productCategoryDataGridViewTextBoxColumn.HeaderText = "PRODUCT CATEGORY";
+            this.productCategoryDataGridViewTextBoxColumn.Name = "productCategoryDataGridViewTextBoxColumn";
+            this.productCategoryDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // productImageDataGridViewImageColumn
+            // 
+            this.productImageDataGridViewImageColumn.DataPropertyName = "ProductImage";
+            this.productImageDataGridViewImageColumn.HeaderText = "PRODUCT IMAGE";
+            this.productImageDataGridViewImageColumn.Name = "productImageDataGridViewImageColumn";
+            this.productImageDataGridViewImageColumn.ReadOnly = true;
+            this.productImageDataGridViewImageColumn.Visible = false;
             // 
             // ProductPrice
             // 
@@ -159,12 +231,15 @@
             this.Name = "ProductPrice";
             this.Text = "Product Price";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.ProductPrice_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -175,8 +250,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.TextBox InvoiceNumber;
-        private System.Windows.Forms.Button SearchInvoice;
+        private System.Windows.Forms.TextBox txtSearchPrice;
+        private System.Windows.Forms.Button SearchPrice;
         private System.Windows.Forms.DataGridView dgv1;
+        private ProductsDataSet productsDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private ProductsDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productPriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productCategoryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn productImageDataGridViewImageColumn;
     }
 }
--- Foodies/ProductPrice.cs
+++ Foodies/ProductPrice.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,82 @@ namespace Foodies
         public ProductPrice()
         {
             InitializeComponent();
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        public void dgv_1()
+        {
+            dgv1.RowTemplate.Height = 40;
+
+            //This Part of Code is for the styling of the Grid Padding
+            Padding newPadding = new Padding(0, 10, 0, 10);
+            this.dgv1.ColumnHeadersDefaultCellStyle.Padding = newPadding;
+
+            //This Part of Code is for the styling of the Grid Columns
+            dgv1.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 10F, FontStyle.Regular);
+            dgv1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            //This Part of Code is for the styling of the Visaul Style
+            //dgv1.EnableHeadersVisualStyles = false;
+
+            // This Part of Code is for the styling of the Grid Border
+            this.dgv1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            this.dgv1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+
+            //This Part of Code is for the styling of the Grid Rows
+            dgv1.RowsDefaultCellStyle.Font = new Font("Arial", 12F, FontStyle.Regular);
+
+            //this Line of Code made the dgv1 Text Middle Center
+            dgv1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            //this line of code is applying padding to a specific Column of dgv1 which is Product Column
+            //dgv2.Columns[4].DefaultCellStyle.Padding = new Padding(3, 3, 3, 3);
+
+        }
+
+        private void ProductPrice_Load(object sender, EventArgs e)
+        {
+            dgv_1();
+            txtSearchPrice.Focus();
+            // TODO: This line of code loads data into the 'productsDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.productsDataSet.Products);
+        }
+
+        private void SearchPrice_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if(txtSearchPrice.Text == string.Empty)
+                {
+                    MessageBox.Show("Product name cannot be blank");
+                    txtSearchPrice.Focus();
+                }
+                else
+                {
+                    SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-9CBGPDG\ASHIRAFZAL;Initial Catalog=foodtime;Integrated Security=True;Pooling=False");
+                    con.Open();
+                    string query = "select * from Products where ProductName = '" + txtSearchPrice.Text + "' ";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    dgv1.DataSource = dt;
+                    con.Close();
+
+                    txtSearchPrice.Text = string.Empty;
+                    txtSearchPrice.Focus();
+                }
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error searching product name");
+            }
         }
     }
 }
