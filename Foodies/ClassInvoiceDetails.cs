ea4ecffaf21ad1c803ecbf33c4fcb8b3d8c6e2fe
ashirafzal <ashirafzal96@gmail.com>
2019-09-05 23:16:59 +0500

    Edit Invoice  C# class added and coded

---
+++ Foodies/ClassInvoiceDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodies
+{
+    class ClassInvoiceDetails
+    {
+        public int InvoiceID { get; set; }
+        public int CustID { get; set; }
+        public int OrderID { get; set; }
+        public string CustName { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductQuantity { get; set; }
+        public decimal ProductRate { get; set; }
+        public decimal ProductAmount { get; set; }
+        public decimal ProductAmountWithGST { get; set; }
+        public TimeSpan OrdertTime { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Totalqty { get; set; }
+        public decimal ActualAmount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal TotalAmountWithGST { get; set; }
+        public decimal DiscountInPercent { get; set; }
+    }
+}
